fd79c6829e29678300aefcab2e8d2e28a4db7c1c
AsianBotV1 <104330520+AsianBotV1@users.noreply.github.com>
2022-05-23 17:07:20 -0500

    Update index.js

--- index.js
+++ index.js
@@ -16,12 +16,12 @@ const { name, author } = require(join(__dirname, './package.json')) // https://w
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
 
-say('DyLux\nWhatsApp Bot', {
+say('Asian\nWhatsApp Bot', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']
 })
-say(`'${name}' By @fg98._`, {
+say(`'${name}' By @IlliaB._`, {
   font: 'console',
   align: 'center',
   gradient: ['red', 'magenta']
