1d0b56a1425688d986647c926c81190279d7f4a3
AsianBotV1 <104330520+AsianBotV1@users.noreply.github.com>
2022-05-23 17:39:39 -0500

    Update menu2.js

--- plugins/menu2.js
+++ plugins/menu2.js
@@ -22,7 +22,7 @@ envíame por t.me/fgsupp_bot el *audio + comando* con en el que responderá
 └──────────────`
 const pp = await (await fetch('https://i.ibb.co/qMG1JPY/fg.jpg')).buffer()
     
-    conn.sendHydrated(m.chat, m2, '▢ DyLux  ┃ ᴮᴼᵀ\n▢ Sígueme en Instagram\nhttps://www.instagram.com/fg98._\n', pp, 'https://youtube.com/fg98f', 'YouTube', null, null, [
+    conn.sendHydrated(m.chat, m2, '▢ IlliaB 𖠌┃ ᴮᴼᵀ\n▢ Sígueme en Instagram\nhttps://www.instagram.com._\n', pp, 'https://youtube.com', 'YouTube', null, null, [
      ['⏍ Info', '/botinfo'],
       ['⌬ Grupos', '/gpdylux']
     ], m)
